7154083d97083d99a16c9c61c41ab35fc71b4208
VladParaschiv <vladparaschiv03@gmail.com>
2020-11-20 16:16:51 +0200

    Update la stergerea tabelelor (am adaugat si stergerea la sequence)

--- scripturi/StergereTabele.sql
+++ scripturi/StergereTabele.sql
@@ -4,3 +4,8 @@ drop table locuri_de_parcare;
 drop table proprietari;
 drop table strazi;
 drop table modalitati_plata;
+
+DROP SEQUENCE ID_PLATA_SEQ;
+DROP SEQUENCE INDEX_LOC_PARCARE_SEQ;
+DROP SEQUENCE INDEX_PROPRIETAR_SEQ;
+DROP SEQUENCE INDEX_STRADA_SEQ;
